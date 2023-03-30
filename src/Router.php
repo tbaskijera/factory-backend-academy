bfd3cdfb4896bee9a9f546cb32f67425b2abc7c7
Toni Baskijera <tonibaskijera@gmail.com>
2023-03-30 09:15:19 +0200

    Add Router.php

---
+++ src/Router.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+class Router {
+    private static array $routes;
+    private static array $validMethods = ["GET", "POST"]; # "PUT", "DELETE"
+
+    public static function addRoute($method, $url, $callback): void
+    {
+        try {
+            if (!in_array($method, self::$validMethods)) {
+                throw new Exception("Invalid HTTP method: $method");
+            }
+
+            $route = [
+                "method" => $method,
+                "url" => $url,
+                "callback" => $callback
+            ];
+
+            self::$routes[] = $route;
+        } catch (Exception $e) {
+            echo "Error: " . $e->getMessage();
+        }
+
+    }
+
+    public static function resolveRoute(Request $request): ?Response
+    {
+        $method = $request->getMethod();
+        $path = $request->getPath();
+
+        foreach (self::$routes as $route) {
+            if ($route["method"] === $method && $route["url"] === $path) {
+                $callback = $route["callback"];
+                return $callback($request);
+            }
+        }
+
+        return null;
+    }
+}
