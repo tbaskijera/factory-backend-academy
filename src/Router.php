37f7420d8a6aabb2154f364face8efc2bcc3046a
Toni Baskijera <tonibaskijera@gmail.com>
2023-04-02 15:58:11 +0200

    Extract parameters, handle controllers

--- src/Router.php
+++ src/Router.php
@@ -7,7 +7,7 @@ use Exception;
 
 class Router
 {
-    private static array $routes;
+    private static array $routes = [];
     private static array $validMethods = ['GET', 'POST']; # 'PUT', 'DELETE'
 
     public static function addRoute($method, $url, $callback): void
@@ -17,12 +17,45 @@ class Router
                 throw new Exception('Invalid HTTP method: $method');
             }
 
+            // Parse the URL and extract parameter names (from Chat GPT)
+            preg_match_all('/\{(\w+)\}/', $url, $matches);
+            $params = $matches[1];
+            $urlPattern = preg_replace('/\{(\w+)\}/', '(\w+)', $url);
+
+            if (is_string($callback)) {
+                // If the callback is a string, assume it's a controller method and refactor it
+                // ex. 'IndexController@indexJsonAction'
+                if (str_contains($callback, '@')) {
+                    [$controllerName, $controllerMethod] = explode('@', $callback);
+                    $controller = 'App\\Controllers\\'.$controllerName; # temporary, bad solution, use dependency injection container?
+                    $callback = [$controller, $controllerMethod];
+                }
+            }
+
+            if (is_array($callback)) {
+                // Check if the callback is an array in the form ['ControllerName', 'methodName']
+                // ex. [IndexController::class, 'indexAction']
+                // ex. 'IndexController@indexJsonAction' also ends up here because of previous processing
+                if (count($callback) !== 2 || !is_string($callback[0]) || !is_string($callback[1])) {
+                    throw new Exception('Invalid callback: ' . print_r($callback, true));
+                }
+
+                // Create a callable from the array, assuming the first element is the controller class name and the second is the method name
+                $controllerName = $callback[0];
+                $controllerMethod = $callback[1];
+                $callback = function ($request) use ($controllerName, $controllerMethod) {
+                    $controller = new $controllerName();
+                    return $controller->$controllerMethod($request);
+                };
+            }
+
             $route = [
                 'method' => $method,
                 'url' => $url,
-                'callback' => $callback
+                'callback' => $callback,
+                'params' => $params,
+                'urlPattern' => $urlPattern,
             ];
-
             self::$routes[] = $route;
         } catch (Exception $e) {
             echo 'Error: ' . $e->getMessage();
@@ -32,15 +65,20 @@ class Router
     public static function resolveRoute(Request $request): ?ResponseInterface
     {
         $method = $request->getMethod();
-        $uri = $request->getPath();
+        $uri = $request->getUri();
 
         foreach (self::$routes as $route) {
-            if ($route['method'] === $method && $route['url'] === $uri) {
-                $callback = $route['callback'];
-                return $callback($request);
+            if ($route['method'] === $method) {
+                $pattern = '/^' . str_replace('/', '\/', $route['urlPattern']) . '$/';
+                if (preg_match($pattern, $uri, $matches)) {
+                    $params = array_combine($route['params'], array_slice($matches, 1));
+                    $request->setParams($params);
+
+                    $callback = $route['callback'];
+                    return $callback($request);
+                }
             }
         }
-
         return null;
     }
 }
