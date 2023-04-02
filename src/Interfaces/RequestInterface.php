f89d6c9ac959348ab2ce9bb4453fb28005616056
Toni Baskijera <tonibaskijera@gmail.com>
2023-04-02 15:58:49 +0200

    Add setParams method

--- src/Interfaces/RequestInterface.php
+++ src/Interfaces/RequestInterface.php
@@ -9,4 +9,5 @@ interface RequestInterface
     public function getHeaders(): array;
     public function getBody(): string;
     public function getParams(): array;
+    public function setParams(array $params): void;
 }
