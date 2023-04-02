0c9cbbda0bd866c384fd4d56d049324e6ea76372
Toni Baskijera <tonibaskijera@gmail.com>
2023-04-02 15:57:35 +0200

    Add different types of routes

--- src/routes.php
+++ src/routes.php
@@ -1,14 +1,32 @@
 <?php
 
+use App\Controllers\IndexController;
+use App\JsonResponse;
 use App\Router;
 use App\Response;
 use App\Interfaces\RequestInterface;
 use App\Interfaces\ResponseInterface;
 
+Router::addRoute('GET', '/index', [IndexController::class, 'indexAction']);
+Router::addRoute('GET', '/index/json', 'IndexController@indexJsonAction');
+
+# Normal callbacks also work
+/*
+require('books.php');
 Router::addRoute('GET', '/index', function (RequestInterface $request): ResponseInterface {
-    return new Response('GET method');
+    return new Response(var_dump(getBooks()));
 });
 
-Router::addRoute('POST', 'index', function (RequestInterface $request): ResponseInterface {
-    return new Response('POST method');
+Router::addRoute('GET', '/index/{productId}', function (RequestInterface $request): ResponseInterface {
+    $books = getBooks();
+    foreach ($books as $book) {
+        if ($book['id'] == $request->getParams()['productId']) {
+            return new Response(var_dump($book));
+        }
+    }
+    return new Response("Not found");
 });
+Router::addRoute('GET', '/index/{brand}/{model}/{id}', function (RequestInterface $request): JsonResponse {
+    return new JsonResponse($request->getParams());
+});
+*/
