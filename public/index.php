3364e5284f96a04f1561fb392996db9e97ba92a8
Toni Baskijera <tonibaskijera@gmail.com>
2023-04-12 09:54:35 +0200

    Init class

--- public/index.php
+++ public/index.php
@@ -5,9 +5,11 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use App\Router;
 use App\Request;
+use App\Models\User;
 
 require('../src/routes.php');
 
+User::__init();
 $request = new Request();
 $response = Router::resolveRoute($request);
 echo $response->send();
